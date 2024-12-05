800213ccd95d74c87e44b77d4dc7a45bec015f46
Jana Filipenská <jana.filipenska@xitee.com>
2024-12-05 21:34:28 +0100

    přidání třídy

--- JanaTextField.js
+++ JanaTextField.js
@@ -1,24 +1,30 @@
 ﻿const TextField = Formio.Components.components.textfield;
 
 class JanaTextField extends TextField {
-	static get builderInfo() {
-		return {
-			title: 'Jana Text Field',
-			group: 'basic',
-			icon: 'fa fa-font',
-			weight: 0,
-			schema: JanaTextField.schema()
-		};
-	}
+    static get builderInfo() {
+        return {
+            title: 'Jana Text Field',
+            group: 'basic',
+            icon: 'fa fa-font',
+            weight: 0,
+            schema: JanaTextField.schema()
+        };
+    }
 
-	static schema(...extend) {
-		return TextField.schema({
-			type: 'janatextfield',
-			label: 'Jana Text Field',
-			key: 'janatextfield',
-			inputType: 'text'
-		}, ...extend);
-	}
+    static schema(...extend) {
+        return TextField.schema({
+            type: 'janatextfield',
+            label: 'Jana Text Field',
+            key: 'janatextfield',
+            inputType: 'text'
+        }, ...extend);
+    }
+
+    inputInfo(input) {
+        const info = super.inputInfo(input);
+        info.class += 'xitee'; 
+        return info;
+    }
 }
 console.log('Registrace komponenty JanaTextField proběhla 5');
 Formio.Components.addComponent('janatextfield', JanaTextField);
